f516d946bda4e70c57f96f2659d26a8dbea3c598
doyeonkim1997 <0104801@naver.com>
2025-06-15 23:56:55 +0900

    ✨ 유데미 JS 파트

---
+++ personal-study/06-15 개인공부.js
@@ -0,0 +1,22 @@
+let random = Math.random();
+if (random < 0.5) {
+  console.log("The number is less than 0.5");
+} else {
+  console.log("숫자가 0.5보다 같거나 큽니다.");
+}
+console.log("랜덤 숫자:", random);
+
+const age = -89;
+
+if (age < 5) {
+  console.log("You are a toddler.");
+} else if (age < 13) {
+  console.log("You are a child.");
+} else if (age < 20) {
+  console.log("You are a teenager.");
+} else {
+  console.log("You are an adult.");
+}
+
+
+
