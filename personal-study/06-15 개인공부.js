ac91f07757b1fe9dd8aa94f8171c224035b04ea8
doyeonkim1997 <0104801@naver.com>
2025-06-16 01:40:59 +0900

    ✨ 유데미 JS 파트

--- personal-study/06-15 개인공부.js
+++ personal-study/06-15 개인공부.js
@@ -1,3 +1,4 @@
+/*
 let random = Math.random();
 if (random < 0.5) {
   console.log("The number is less than 0.5");
@@ -18,5 +19,24 @@ if (age < 5) {
   console.log("You are an adult.");
 }
 
+function getColor(phrase) {
+  //WRITE YOUR CODE BETWEEN THIS LINE: ↓ ↓ ↓ ↓ 
+  if (phrase === 'stop') {
+    console.log('red');
+  } else if (phrase === 'slow') {
+    console.log('yellow');
+  } else if (phrase === 'go') {
+    console.log('green');
+  } else {
+    console.log('purple');
+  }
+}
+*/
 
+const password = prompt('비밀번호를 입력하세요.');
 
+if (password.length >= 8) {
+  console.log('비밀번호는 강력합니다.');
+} else {
+  console.log('비밀번호는 약합니다. 최소 8자 이상이어야 합니다.');
+}
