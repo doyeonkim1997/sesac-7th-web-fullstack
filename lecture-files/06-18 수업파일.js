91c9050b5bc62eccaf3d70f449687a9bf90223ef
doyeonkim1997 <0104801@naver.com>
2025-06-18 15:23:30 +0900

    💡 JS 코드 스타일 정리 및 네이밍 규칙 주석 추가

---
+++ lecture-files/06-18 수업파일.js
@@ -0,0 +1,57 @@
+// // function great() {
+// //   console.log('Great!');
+// // }
+
+// // function great2() {
+// //   console.log('Great2!');
+// // }
+
+// // 하위 개념일 때는 띄어쓰기 해주는 게 좋음
+// // visual studio code에서는 자동으로 띄어쓰기를 해줌 (저장하면 자동으로 띄어쓰기 됨)
+
+// const userList = []
+// // 유저 이름이 들어가있을거같음
+// // 앞은 명사로 작성, 뒤는 동사로 작성
+
+// const list = [] // 어떤 정보가 들어있는지 직접 값을 확인해야함
+
+// function getUserName() {
+
+// }
+// // 함수는 첫글자로 소문자+동사로 시작(동작한다는 개념)
+
+
+// function greet() {
+//   console.log("Hello");
+// }
+// // 좋음
+
+// function greet() {
+//   console.log("Hello");
+// }
+// // 나쁨
+
+
+// if (!user.isLoggedIn) {
+//   redirect("/");
+// }
+// // 사용자가 로그인하지 않았을 때 홈으로 리디렉션
+
+// const userAge = 25;
+// // 변수는 소문자로 시작, 카멜케이스로 작성
+
+// const isLoggedIn = true;
+// // 변수는 명사로 작성, 동사로 시작하지 않음
+
+
+// const map = new Map();
+// // Map은 키와 값을 쌍으로 저장하는 자료구조
+
+// map.set("key1", "value1");
+// map.set("key2", "value2");
+
+// console.log(map.has("key1")); // true
+// console.log(map.has("key2")); // true
+
+
+
