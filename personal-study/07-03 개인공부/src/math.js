21ca2f13b7f19a7cff58ea885c21b8d7ca9f598f
doyeonkim1997 <0104801@naver.com>
2025-07-03 21:57:57 +0900

    🍪 한 입 크기로 잘라 먹는 리액트: Node.js 입문, React 입문 Day 8,9

---
+++ personal-study/07-03 개인공부/src/math.js
@@ -0,0 +1,24 @@
+
+// 계산기 기능 math 모듈
+
+
+export function add(a, b) {
+  return a + b;
+}
+
+export function sub(a, b) {
+  return a - b;
+}
+
+export default function multiply(a, b) {
+  return a * b;
+}
+
+// // 모듈을 내보내는 방법
+// module.exports = {
+//   add,
+//   sub,
+// };
+
+// 객체를 리터럴로 담아서 내보내는 방법
+// export {add , sub};
